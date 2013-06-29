f5343a6891bc60850082d509b988198d50c31e81
luofei2011 <luofeihit2010@gmail.com>
2013-06-29 15:31:57 +0800

    基本完工

---
+++ js/data.js
@@ -0,0 +1,153 @@
+var data = {
+	'today_live': [
+		{
+			'title': '纪念逝去的绿皮火车：6417',
+			'content': "有一趟火车，从1960年开始服役。自北京站出发，途径通州、顺义、怀柔等，最终到达承德。全程6小时24分钟，票价17元。而从北京站到通州西只需要1.5元，十八公里，半个小时，不堵车，不晚点，有座。这就是北京最便宜的绿皮火车。6417。2012年6月30日，下午4时22分许，最后一趟6417次列车从北京站驶出，完成其在北京地区的最后服役。2012年7月1日，该趟列车将由K7717/K7718次列车取代。新车将不再停靠通州西站，新车在北京境内仅停靠顺义、怀柔、密云三站。30日下午，墙根网（www.qianggen.com）俱乐部的五位同学踏上了这趟列车，用相机记录下了最后的6417。本期，就和大家一起分享、纪念一下。希望大家喜欢。如果感兴趣可以随时在墙根网官方微博（http://weibo.com/qianggen）与我们交流。",
+			'url': './img/bak/duobei_52.png',
+			'author_photo': './img/photo/duobei_41.png',
+			'author_name': '墙根网',
+			'pub_time': '今天晚上20:30',
+			'is_live': true
+		},
+		{
+			'title': '新浪微博运营经理：如何做好用户运营',
+			'content': '用户运营的核心，是把活跃用户的规模往上提，方式有两种：开源和节流，开的是注册的源、节的是流失的流。还有一种，那就是保活跃，让不活跃用户变活跃、让 活跃用户更活跃。本课程据此分为三个部分：《开源：如何提高注册用户量》，《节流：如何留住注册用户》和《疯狂的运营：如何让用户保持活跃》。',
+			'url': './img/bak/duobei_54.png',
+			'author_photo': './img/photo/duobei_22.png',
+			'author_name': '小妍',
+			'pub_time': '7月6日 21:00',
+			'is_live': false
+		},
+		{
+			'title': '教您如何正确开启葡萄酒和香槟酒瓶',
+			'content': '开启葡萄酒瓶应注意什么？怎样正确开启葡萄酒瓶？如何安全的打开香槟或汽泡酒瓶？@CHEERSWines 葡萄酒讲师昂前老师将视频给大家展示细节。更多精彩请关注 @CHEERSWines 微博 :)',
+			'url': './img/bak/duobei_56.png',
+			'author_photo': './img/photo/duobei_44.png',
+			'author_name': 'CHEERSWines',
+			'pub_time': '7月6日 20:00',
+			'is_live': false
+		}
+	],
+	'internet': [
+		{
+			'title': '如何计算融资后的股权比例及股权价值',
+			'url': './img/bak/duobei_64.png',
+			'author_photo': './img/photo/duobei_06.png',
+			'author_name': '张丽君',
+			'fllow_num': 1342,
+			'is_live': true
+		},
+		{
+			'title': '多贝必修课：如何举办自己的网络公开课',
+			'url': './img/bak/duobei_66.png',
+			'author_photo': './img/photo/duobei_08.png',
+			'author_name': '小田',
+			'fllow_num': 1342,
+			'is_live': true
+		},
+		{
+			'title': '早期互联网创业项目如何撰写商业计划书？',
+			'url': './img/bak/duobei_62.png',
+			'author_photo': './img/photo/duobei_06.png',
+			'author_name': '张丽君',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '新浪微博运营经理：如何做好用户运营',
+			'url': './img/bak/duobei_68.png',
+			'author_photo': './img/photo/duobei_10.png',
+			'author_name': '小妍',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '创新工场公开课：孙陶然和他的创业36条军规',
+			'url': './img/bak/duobei_72.png',
+			'author_photo': './img/photo/duobei_12.png',
+			'author_name': 'IW-Mentoring S',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '阿里学院 李亮：移动电商那些事儿',
+			'url': './img/bak/duobei_76.png',
+			'author_photo': './img/photo/duobei_14.png',
+			'author_name': '阿里学院 李亮',
+			'fllow_num': 1342,
+			'is_live': false
+		}
+	],
+	'abroad': [
+		{
+			'title': '2012全额奖学金申请成功经验分享会',
+			'url': './img/bak/duobei_71.png',
+			'author_photo': './img/photo/duobei_16.png',
+			'author_name': '李笑来',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '托福系列课程之三：阅读的三种重要逻辑',
+			'url': './img/bak/duobei_79.png',
+			'author_photo': './img/photo/duobei_18.png',
+			'author_name': '范琛Sam',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '【雅思阅读】1小时搞定雅思阅读高分的秘诀',
+			'url': './img/bak/duobei_84.png',
+			'author_photo': './img/photo/duobei_20.png',
+			'author_name': '北京艾迪书院',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '2012全额奖学金申请成功经验分享会',
+			'url': './img/bak/duobei_71.png',
+			'author_photo': './img/photo/duobei_16.png',
+			'author_name': '李笑来',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '托福系列课程之三：阅读的三种重要逻辑',
+			'url': './img/bak/duobei_79.png',
+			'author_photo': './img/photo/duobei_18.png',
+			'author_name': '范琛Sam',
+			'fllow_num': 1342,
+			'is_live': false
+		},
+		{
+			'title': '【雅思阅读】1小时搞定雅思阅读高分的秘诀',
+			'url': './img/bak/duobei_84.png',
+			'author_photo': './img/photo/duobei_20.png',
+			'author_name': '北京艾迪书院',
+			'fllow_num': 1342,
+			'is_live': false
+		}
+	],
+	'game_rank': [
+		{
+			'person_photo': './img/photo/duobei_37.png',
+			'person_name': 'Heaton',
+			'description': '擅长使用AK47'
+		},
+		{
+			'person_photo': './img/photo/duobei_35.png',
+			'person_name': '扭曲的机器',
+			'description': '占星师。居帝都。知道自己存在的意义。'
+		},
+		{
+			'person_photo': './img/photo/duobei_39.png',
+			'person_name': '这是谁',
+			'description': '这是一名英语老师吧'
+		},
+		{
+			'person_photo': './img/photo/duobei_33.png',
+			'person_name': '小Y',
+			'description': '走位一定要风骚'
+		}
+	]
+};
--- js/doubei.js
+++ js/doubei.js
@@ -1,10 +1,27 @@
 var $ = function(selector) {
     return document.getElementById(selector);
+};
+var getElementByClassName = function( str ) {
+    var elem = str.slice(0, str.indexOf('.')) || '',  // éè¿ä¼ éçåæ°å¾å°åç´ ååå¯¹åºçç±»å
+        className = str.slice(str.indexOf('.')+1, str.length),
+        result = [],
+        i = 0,
+        reg = new RegExp("\\b" + className + "\\b", 'g'); // éç¨çå¹éåºææ¾ç±»åæ­£åè¡¨è¾¾å¼
+
+    var all = (elem === '') ? document.getElementsByTagName('*') : document.getElementsByTagName(elem); // // å¼å®¹æ²¡æä¼ éç»ç¹åçæåµ
+
+    for ( ; i < all.length; i++ ) {
+        if ( reg.test(all[i].className) ) {
+            result.push(all[i]);
+        }
+    }
+
+    return result;
 }
 function placeholder(element) {
-    var placeholder = '';
-    if (element && !("placeholder" in document.createElement("input")) && 
-            (placeholder = element.getAttribute("placeholder"))) {
+    var placeholder = element.getAttribute('placeholder') ||
+                      element.getAttributeNode('placeholder').nodeValue;
+    if (element && !("placeholder" in document.createElement("input")) && placeholder) {
         var idLabel = element.id ;
         if (!idLabel) {
             idLabel = "placeholder_" + new Date().getTime();
